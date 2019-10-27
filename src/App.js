28b11f86a23c7d0724600ce3171d8e789d0828e5
Ruben Uribe <uriben2000@gmail.com>
2019-10-27 00:35:38 -0700

    api connection

--- src/App.js
+++ src/App.js
@@ -1,40 +1,87 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios';
+import app from 'firebase/app';
+import firebase from 'firebase';
 import DriverCard from './components/DriverCard';
 import { directive } from '@babel/types';
 
 
 
-function App() {
-  return (
-    <>
-    <div className="driverContainer" style={{display:"flex", height: "100vh",alignItems:"center", overflowX: "scroll", width: "100vw"}}>
-          <div className="App">
-            <DriverCard name="Ruben1" speed="20" distance="30" time="600"/>
-          </div>
-          <div className="App">
-            <DriverCard name="Ruben2" speed="20" distance="30" time="600"/>
-          </div>
-          <div className="App">
-            <DriverCard name="Ruben3" speed="20" distance="30" time="600"/>
-          </div>
-          <div className="App">
-            <DriverCard name="Ruben4" speed="20" distance="30" time="600"/>
-          </div>
-          <div className="App">
-            <DriverCard name="Ruben5" speed="20" distance="30" time="600"/>
-          </div>
-          <div className="App">
-            <DriverCard name="Ruben6" speed="20" distance="30" time="600"/>
-          </div>
-          <div style={{zIndex:"-10"}}>.</div>
 
-    </div>
-        <script src="https://www.gstatic.com/firebasejs/7.2.2/firebase-app.js"></script>  
-        <script src="https://www.gstatic.com/firebasejs/7.2.2/firebase-analytics.js"></script>   
-    </>
-  );
-}
 
-export default App;
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      people:[]
+    }
+  }
+  
+  componentDidMount(){
+    var data;    
+    axios.get("https://calhackssix.firebaseio.com/.json")
+    .then(res=>{
+      data=res.data;
+      console.log(data);
+      let newPeople=[];
+      let keys=Object.keys(data);
+      for (let i=0; i<keys.length; i++){
+        let newObj = {};
+        newObj["name"]=keys[i];
+        newObj["speed"]=data[keys[i]]["data2"]["currentSpeed"];
+        newObj["distance"]=data[keys[i]]["data2"]["distanceDriven"];
+        newObj["time"]=data[keys[i]]["data2"]["timeMoving"];
+        newPeople.push(newObj);
+      }
+      this.setState({people: newPeople});
+
+    });  
+  }
+  render(){
+    return (
+        <>
+        <div>
+          <h1></h1>
+        </div>
+        <div className="driverContainer" style={{display:"flex", height: "100vh",alignItems:"center", overflowX: "scroll", width: "100vw"}}>
+              {
+                this.state.people.map((obj) => {
+                  return <DriverCard name={obj.name} speed={obj.speed} distance={obj.distance} time={obj.time}/>
+                })
+              }
+
+              <div style={{zIndex:"-10"}}>.</div>
+
+        </div>  
+        </>
+      );
+    }
+  }
+
+  // Your web app's Firebase configuration
+  // var firebaseConfig = {
+  //   apiKey: "AIzaSyALinEZTQIewu1jIDoW6hM6Dgcj47uRn5k",
+  //   authDomain: "calhackssix.firebaseapp.com",
+  //   databaseURL: "https://calhackssix.firebaseio.com",
+  //   projectId: "calhackssix",
+  //   storageBucket: "calhackssix.appspot.com",
+  //   messagingSenderId: "966064755351",
+  //   appId: "1:966064755351:web:8538ca95dc707152d68fd2",
+  //   measurementId: "G-S4FJJP5FW6"
+  // };
+  // app.initializeApp(firebaseConfig);
+   
+  
+  //  var people = app.database.ref('calhackssix');
+  // console.log(people);
+  // const ref= firebase.database(app).ref('calhackssix');
+  // console.log(ref);
+  // console.log(firebase.database(app).ref(''));
+
+  
+  // console.log(driverArray);
+  // driverArray.forEach(function (driver){
+  //   console.log(driver);
+  // })
--- src/components/DriverCard.js
+++ src/components/DriverCard.js
@@ -1,34 +1,57 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
-
+import axios from 'axios';
 
 export default class DriverCard extends Component{
   constructor(props){
     super(props);
     this.onClick=this.onClick.bind(this);
+    this.updateData=this.updateData.bind(this);
     this.state={
       speed:this.props.speed,
       distance:this.props.distance,
-      time:this.props.time
+      time:this.props.time,
+      moving:this.props.speed>=1
     }
+  }
+  updateData(){
+    //Query the api
+    axios.get(`https://calhackssix.firebaseio.com/${this.props.name}.json`)
+    .then(res=>{
+      console.log(res);
+    });
+
+
+
+    //update the state
+    // this.setState({speed:0,distance:0,time:0});
+    setTimeout(this.updateData, 5000);
+  }
+  componentDidMount(){
+    this.updateData();
   }
   onClick(){
     //handle the click event
+    console.log(this.state.moving);
     console.log(this.props.name);
-    this.setState({speed:0,distance:0,time:0})
-    //post to the api set the data to zero
+    this.setState({speed:0,distance:0,time:0});
+    document.getElementById(this.props.name+"name").classList.remove('green');
   }
+  
+
   
   render(){
     return(
-      <div className="Driver">
-        <h1>{this.props.name}</h1>
-        <br></br>
-        <h3>average speed: {this.state.speed} mph</h3>
-        <h3>distance travled: {this.state.distance} miles</h3>
-        <h3>Time driving: {this.state.time} minutes</h3>
-        <br></br>
-        <Button onClick={this.onClick} id={this.props.name} variant="dark" size="lg" style={{minWidth:"30%"}}>Reset</Button>
+      <div className={(this.props.speed>=1) ? 'App green' : 'App white'} id={this.props.name+"name"}>
+        <div className="Driver">
+          <h1>{this.props.name}</h1>
+          <br></br>
+          <h3>current speed: {this.state.speed} mph</h3>
+          <h3>distance travled: {this.state.distance} miles</h3>
+          <h3>Time driving: {this.state.time} minutes</h3>
+          <br></br>
+          <Button disabled={this.state.moving} onClick={this.onClick} id={this.props.name} variant="dark" size="lg" style={{minWidth:"30%"}}>Reset</Button>
+        </div>
       </div>
     );
   }
